8ff50e318a9d8e16a36863a01f7c39dace0ab05f
hichaniko <hichaniko1127@gmail.com>
2023-09-11 17:17:34 +0900

    商品購入機能の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,15 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!, only:[:index]
+ 
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
+    if current_user != @item.user && !@item.order.present?
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
+    else
+    redirect_to root_path
+   end 
   end
 
 
